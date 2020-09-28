328f31f083e4f32b708d17883bb55812049d6266
Anibal Bastias Soto <anibal.bastias@gmail.com>
2020-09-28 09:02:26 -0300

    Implement Use case unit tests in domain layer

--- library/src/test/java/com/anibalbastias/weatherlib/data/repository/WeatherRepositoryImplTest.kt
+++ library/src/test/java/com/anibalbastias/weatherlib/data/repository/WeatherRepositoryImplTest.kt
@@ -1,7 +1,87 @@
 package com.anibalbastias.weatherlib.data.repository
 
-import org.junit.Assert.*
+import com.anibalbastias.testutils.foundation.RandomFactory
+import com.anibalbastias.weatherlib.data.mapper.CurrentConditionsMapper
+import com.anibalbastias.weatherlib.data.mapper.ForecastMapper
+import com.anibalbastias.weatherlib.data.mapper.GeoPositionMapper
+import com.anibalbastias.weatherlib.data.retrofit.service.WeatherService
+import com.anibalbastias.weatherlib.factory.CurrentConditionFactory
+import com.anibalbastias.weatherlib.factory.ForecastFactory
+import com.anibalbastias.weatherlib.factory.GeoLocationFactory
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import org.junit.Before
+import org.junit.Test
 
 class WeatherRepositoryImplTest {
 
+    @MockK
+    internal lateinit var mockService: WeatherService
+    private lateinit var geoPositionMapper: GeoPositionMapper
+    private lateinit var currentConditionsMapper: CurrentConditionsMapper
+    private lateinit var forecastMapper: ForecastMapper
+    private lateinit var cut: WeatherRepositoryImpl
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        geoPositionMapper = GeoPositionMapper()
+        currentConditionsMapper = CurrentConditionsMapper()
+        forecastMapper = ForecastMapper()
+        cut = WeatherRepositoryImpl(
+            mockService, geoPositionMapper, currentConditionsMapper, forecastMapper
+        )
+    }
+
+    @Test
+    fun `getGeoPosition fetches DomainGeoLocation and maps to RemoteGeoLocation`() {
+        // given
+        val latitude = RandomFactory.generateDouble()
+        val longitude = RandomFactory.generateDouble()
+        val query = "$latitude,$longitude"
+
+        coEvery {
+            cut.getGeoPosition(query)
+        } returns with(geoPositionMapper) {
+            GeoLocationFactory.makeRemoteGeoLocation().fromRemoteToDomain()
+        }
+    }
+
+    @Test
+    fun `getCurrentConditions fetches DomainCurrentCondition and maps to RemoteCurrentCondition`() {
+        // given
+        val cityKey = "${RandomFactory.generateInt()}"
+
+        coEvery {
+            cut.getCurrentConditions(cityKey)
+        } returns with(currentConditionsMapper) {
+            CurrentConditionFactory.makeRemoteCurrentCondition().fromRemoteToDomain()
+        }
+    }
+
+    @Test
+    fun `get1DayDailyForecasts fetches DomainCurrentCondition and maps to RemoteCurrentCondition`() {
+        // given
+        val cityKey = "${RandomFactory.generateInt()}"
+
+        coEvery {
+            cut.get1DayDailyForecasts(cityKey)
+        } returns with(forecastMapper) {
+            ForecastFactory.makeRemoteForecast().fromRemoteToDomain()
+        }
+    }
+
+    @Test
+    fun `get5DayDailyForecasts fetches DomainCurrentCondition and maps to RemoteCurrentCondition`() {
+        // given
+        val cityKey = "${RandomFactory.generateInt()}"
+
+        coEvery {
+            cut.get5DayDailyForecasts(cityKey)
+        } returns with(forecastMapper) {
+            ForecastFactory.makeRemoteForecast().fromRemoteToDomain()
+        }
+    }
 }
--- library/src/test/java/com/anibalbastias/weatherlib/domain/usecase/GetCurrentConditionsUseCaseTest.kt
+++ library/src/test/java/com/anibalbastias/weatherlib/domain/usecase/GetCurrentConditionsUseCaseTest.kt
@@ -1,5 +1,47 @@
 package com.anibalbastias.weatherlib.domain.usecase
 
-import org.junit.Assert.*
+import com.anibalbastias.testutils.foundation.RandomFactory
+import com.anibalbastias.weatherlib.data.repository.WeatherRepositoryImpl
+import com.anibalbastias.weatherlib.factory.CurrentConditionFactory
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
-class GetCurrentConditionsUseCaseTest
+@RunWith(JUnit4::class)
+class GetCurrentConditionsUseCaseTest {
+
+    @MockK
+    internal lateinit var mockRemoteRepository: WeatherRepositoryImpl
+
+    private lateinit var cut: GetCurrentConditionsUseCase
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        cut = GetCurrentConditionsUseCase(mockRemoteRepository)
+    }
+
+    @Test
+    fun `return DomainCurrentCondition`() {
+        // given
+        val cityKey = "${RandomFactory.generateInt()}"
+        val domain = CurrentConditionFactory.makeDomainCurrentCondition()
+
+        coEvery {
+            mockRemoteRepository.getCurrentConditions(cityKey)
+        } returns domain
+
+        // when
+        val result = runBlocking { cut.execute(cityKey) }
+
+        // then
+        result shouldBeEqualTo domain
+    }
+}
